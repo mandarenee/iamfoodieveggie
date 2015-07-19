0e4dfed7ca8b8488255e1ddc0425934df1077c5e
Amanda Kom <amanda.kom@stashrewards.com>
2015-07-19 13:35:33 -0700

    updating search bar and adding large image option

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ActiveRecord::Base
-	has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :photo, :styles => { :large => "400x400>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+
+  def self.search(query)
+		where("body like ? or title like ?", "%#{query}%", "%#{query}%")
+	end
 end
